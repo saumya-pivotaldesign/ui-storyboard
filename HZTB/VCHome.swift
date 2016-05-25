fd1ef2861a7cb8dbd714a681ae3a9f91c376ec0e
saumya <saumya@pivotaldesign.biz>
2016-05-25 12:58:09 +0530

    slider menu implementing

--- HZTB/VCHome.swift
+++ HZTB/VCHome.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
+// MARK: VCHome is ViewController
 class VCHome: UIViewController {
+    
+    let interactor = Interactor()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //
@@ -16,5 +20,48 @@ class VCHome: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         //
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let destinationViewController = segue.destinationViewController as? MenuViewController {
+            destinationViewController.transitioningDelegate = self
+            // 2
+            destinationViewController.interactor = interactor
+        }
+    }
+    
+    // MARK: Expose to Storyboard
+    @IBAction func openMenu(sender: AnyObject) {
+        performSegueWithIdentifier("openMenu", sender: nil)
+    }
+    @IBAction func edgePanGesture(sender: UIScreenEdgePanGestureRecognizer) {
+        let translation = sender.translationInView(view)
+        
+        let progress = MenuHelper.calculateProgress(translation, viewBounds: view.bounds, direction: .Right)
+        
+        MenuHelper.mapGestureStateToInteractor(
+            sender.state,
+            progress: progress,
+            interactor: interactor){
+                self.performSegueWithIdentifier("openMenu", sender: nil)
+        }
+    }
+}
+
+// MARK: Extension of the VCHome
+extension VCHome: UIViewControllerTransitioningDelegate {
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return PresentMenuAnimator()
+    }
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return DismissMenuAnimator()
+    }
+    // 3
+    func interactionControllerForDismissal(animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactor.hasStarted ? interactor : nil
+    }
+    
+    func interactionControllerForPresentation(animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactor.hasStarted ? interactor : nil
     }
 }
